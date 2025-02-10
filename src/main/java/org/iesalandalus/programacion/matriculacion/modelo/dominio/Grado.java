76d9a4a5810420bea194b0c6e0db54baea91cb5e
Manuel <mlc280803@gmail.com>
2025-02-10 14:10:45 +0100

    Clase GradoD creada.

--- src/main/java/org/iesalandalus/programacion/matriculacion/modelo/dominio/Grado.java
+++ src/main/java/org/iesalandalus/programacion/matriculacion/modelo/dominio/Grado.java
@@ -1,20 +1,54 @@
 package org.iesalandalus.programacion.matriculacion.modelo.dominio;
 
-public enum Grado {
-    GDCFGB("GDCFGB"), GDCFGM("GDCFGM"), GDCFGS("GDCFGS");
+public abstract class Grado {
 
-    private String cadenaAMostrar;
+    protected String nombre;
+    protected String iniciales;
+    protected int numAnios;
 
-    private Grado(String cadenaAMostrar) {
-        this.cadenaAMostrar = cadenaAMostrar;
+    public Grado(String nombre) {
+        if (nombre == null) {
+            throw new NullPointerException("ERROR: El nombre de un grado no puede ser nulo.");
+        }
+        if (nombre.isBlank()) {
+            throw new IllegalArgumentException("ERROR: El nombre de un grado no puede estar vacío.");
+        }
+
+        setNombre(nombre);
     }
 
-    public String imprimir() {
-        return String.format("%d .- %s", this.ordinal(), this.cadenaAMostrar);
+    public String getNombre() {
+        return nombre;
     }
+
+    protected void setNombre(String nombre) {
+        if (nombre == null) {
+            throw new NullPointerException("ERROR: El nombre de un grado no puede ser nulo.");
+        }
+        if (nombre.isBlank()) {
+            throw new IllegalArgumentException("ERROR: El nombre de un grado no puede estar vacío.");
+        }
+        this.nombre = nombre;
+    }
+
+    private void setIniciales() {
+
+        StringBuilder iniciales = new StringBuilder();
+        String[] palabras = nombre.split("\\s+");
+
+        for (String palabra : palabras) {
+            if (!palabra.isEmpty()) {
+                iniciales.append(palabra.charAt(0));
+            }
+        }
+
+        this.iniciales = iniciales.toString().toUpperCase();
+    }
+
+    public abstract void setNumAnios(int numAnios);
 
     @Override
     public String toString() {
-        return cadenaAMostrar;
+        return "(" + iniciales + ") - " + nombre;
     }
 }
